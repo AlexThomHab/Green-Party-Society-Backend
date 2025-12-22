afb2346e015a7770cacce9d27bc8488079680fbc
AlexThomHab <alex.haberfield@gmail.com>
2025-12-22 10:23:59 +0000

    added jwt config and removed swagger

--- GreenPartySocietyAPI/Program.cs
+++ GreenPartySocietyAPI/Program.cs
@@ -1,8 +1,14 @@
+using GreenPartySocietyAPI.Repositories;
+using GreenPartySocietyAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddScoped<IPasswordHasher, BcryptPasswordHasher>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IJwtService, JwtService>();
 
 var app = builder.Build();
 
